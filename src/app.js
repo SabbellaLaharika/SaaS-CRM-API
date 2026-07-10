aa6fba592bc24d3acc24b5079e73aba7dbf64238
SabbellaLaharika <22A91A05J9@aec.edu.in>
2026-07-10 20:45:20 +0530

    Expose interactive Swagger UI on /api-docs and serve swagger.yaml

--- src/app.js
+++ src/app.js
@@ -5,10 +5,54 @@ const orgController = require('./controllers/orgController');
 const crmController = require('./controllers/crmController');
 require('dotenv').config();
 
+const path = require('path');
+
 const app = express();
 
 // Parse request bodies
 app.use(express.json());
+
+// Serve Swagger specification
+app.get('/swagger.yaml', (req, res) => {
+  res.sendFile(path.join(__dirname, '../swagger.yaml'));
+});
+
+// Serve interactive API Docs
+app.get('/api-docs', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Multi-Tenant SaaS CRM API - Swagger Docs</title>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+      <style>
+        html { box-sizing: border-box; overflow: -y-scroll; }
+        *, *:before, *:after { box-sizing: inherit; }
+        body { margin: 0; background: #fafafa; }
+      </style>
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"></script>
+      <script>
+        window.onload = () => {
+          window.ui = SwaggerUIBundle({
+            url: '/swagger.yaml',
+            dom_id: '#swagger-ui',
+            deepLinking: true,
+            presets: [
+              SwaggerUIBundle.presets.apis
+            ],
+            layout: "BaseLayout"
+          });
+        };
+      </script>
+    </body>
+    </html>
+  `);
+});
 
 // Mount routers
 app.use('/api/auth', authController);
